6049420e2195d2d22275a55fa2b70fa79ae3d440
Oscar Rene Cuellar Mancilla <usackoka@gmail.com>
2020-08-15 16:31:25 -0600

    Update crud.component.js

--- src/modules/module.privados/components/crud.component.js
+++ src/modules/module.privados/components/crud.component.js
@@ -29,43 +29,6 @@ const CRUDPrivados = props =>{
           title="Listado de Privados"
           columns={state.columns}
           data={state.data}
-          editable={{
-            onRowAdd: (newData) =>
-              new Promise((resolve) => {
-                setTimeout(() => {
-                  resolve();
-                  setState((prevState) => {
-                    const data = [...prevState.data];
-                    data.push(newData);
-                    return { ...prevState, data };
-                  });
-                }, 600);
-              }),
-            onRowUpdate: (newData, oldData) =>
-              new Promise((resolve) => {
-                setTimeout(() => {
-                  resolve();
-                  if (oldData) {
-                    setState((prevState) => {
-                      const data = [...prevState.data];
-                      data[data.indexOf(oldData)] = newData;
-                      return { ...prevState, data };
-                    });
-                  }
-                }, 600);
-              }),
-            onRowDelete: (oldData) =>
-              new Promise((resolve) => {
-                setTimeout(() => {
-                  resolve();
-                  setState((prevState) => {
-                    const data = [...prevState.data];
-                    data.splice(data.indexOf(oldData), 1);
-                    return { ...prevState, data };
-                  });
-                }, 600);
-              }),
-          }}
         />
       );
 }
