b1226f229af26a9a4b35fff06aaa6a4764bb6f86
Oscar Rene Cuellar Mancilla <usackoka@gmail.com>
2020-08-15 15:16:31 -0600

    Menu by material UI

---
+++ src/components/menuBar/material.styles.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+export const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+export const StyledMenu = withStyles({
+    paper: {
+        border: '1px solid #d3d4d5',
+    },
+})((props) => (
+    <Menu
+        elevation={0}
+        getContentAnchorEl={null}
+        anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+        }}
+        transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+        }}
+        {...props}
+    />
+));
+
+export const StyledMenuItem = withStyles((theme) => ({
+    root: {
+        '&:focus': {
+            backgroundColor: theme.palette.primary.main,
+            '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+                color: theme.palette.common.white,
+            },
+        },
+    },
+}))(MenuItem);
--- src/components/menuBar/menuBar.js
+++ src/components/menuBar/menuBar.js
@@ -1,8 +1,71 @@
 import React from 'react'
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import SendIcon from '@material-ui/icons/Send';
+
+import { StyledMenu, StyledMenuItem, useStyles } from './material.styles'
 
 const MenuBar = props => {
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const classes = useStyles();
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
-        <div>MENU</div>
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon onClick={handleClick} />
+                    </IconButton>
+                    <StyledMenu
+                        id="customized-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        <StyledMenuItem>
+                            <ListItemIcon>
+                                <SendIcon fontSize="small" />
+                            </ListItemIcon>
+                            <ListItemText primary="Sent mail" />
+                        </StyledMenuItem>
+                        <StyledMenuItem>
+                            <ListItemIcon>
+                                <DraftsIcon fontSize="small" />
+                            </ListItemIcon>
+                            <ListItemText primary="Drafts" />
+                        </StyledMenuItem>
+                        <StyledMenuItem>
+                            <ListItemIcon>
+                                <InboxIcon fontSize="small" />
+                            </ListItemIcon>
+                            <ListItemText primary="Inbox" />
+                        </StyledMenuItem>
+                    </StyledMenu>
+
+                    <Typography variant="h6" className={classes.title}>Menu</Typography>
+                    <Button color="inherit">HOME</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
     );
 }
 
